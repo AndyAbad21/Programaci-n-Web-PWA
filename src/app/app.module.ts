5a25f80793b723161a4b9ca7d2f782d9f82e3552
AndyPanda2002 <aabadf@est.ups.edu.ec>
2023-12-05 18:53:59 -0500

    Actualizacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { environment } from 'src/environments/environment';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { FirestoreModule } from '@angular/fire/firestore';  // Cambié AngularFirestoreModule a FirestoreModule
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideAuth(() => getAuth()),
+    FirestoreModule,  // Cambié AngularFirestoreModule a FirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
+
---
+++ src/environments/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+    production: false,
+    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+    firebaseConfig:{
+      apiKey: "AIzaSyDUL8MljY6qGxpKnOZfmmE-l8Qn-PdRunU",
+      authDomain: "proyecto-e53e0.firebaseapp.com",
+      projectId: "proyecto-e53e0",
+      storageBucket: "proyecto-e53e0.appspot.com",
+      messagingSenderId: "619628939478",
+      appId: "1:619628939478:web:dfe722b653e5827327a177",
+      measurementId: "G-V81NWXLTTR"
+    }
+  };
+  
